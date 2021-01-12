804e06785b87b598284191fbebab8297b2960c78
Michael Badinger <mcbadinger@gmail.com>
2021-01-12 16:50:54 -0600

    Got drop down working

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,5 +1,14 @@
-// Pull in data
-d3.json("samples.json").then((importedData) => {
-    console.log(importedData);
-    var data = importedData;
+
+
+// Drop Down Menu
+var drop_down = d3.select("#selDataset");
+
+    // connects to the JSON data
+d3.json("samples.json").then((data)=> {
+    console.log(data)
+
+    // adds name values to the dropdown menu
+    data.names.forEach(function(name) {
+        drop_down.append("option").text(name).property("value");
+    });
 });
