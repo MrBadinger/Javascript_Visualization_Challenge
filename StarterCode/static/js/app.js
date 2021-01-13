c241a3f38f8625b7353fe050a80581b1146aad95
Michael Badinger <mcbadinger@gmail.com>
2021-01-12 20:12:31 -0600

    Finish Bar Chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -11,6 +11,8 @@ d3.json("samples.json").then((data)=> {
 
     // Connect the other fuctions to the value in drop down
     demographics(data.names[0])
+    bar_chart(data.names[0])
+
 });
 
 
@@ -36,3 +38,62 @@ function demographics(id) {
         });
     });
 }
+
+
+// Bar chart
+function bar_chart(id) {
+    d3.json("samples.json").then((data)=> {
+
+
+        // grab the sample data of the person choosen in the dropdown
+        var subject_sample = data.samples.filter(sample => sample.id.toString() === id)[0];
+        
+        // orangize sample data from largest to smallest and collect top 10
+        var subject_sample_data = subject_sample.sample_values.slice(0, 10).reverse();
+  
+        // orangize out ids from largest to smallest and collect top 10
+        var top_samples = (subject_sample.otu_ids.slice(0, 10)).reverse();
+        
+        // organize top otu labels from filtered data and collect top 10
+        var label = subject_sample.otu_labels.slice(0, 10);
+
+        // format otu lables
+        var top_samples_2 = top_samples.map(id => "OTU " + id + " ")
+    
+
+        // define the bar chart values
+        var traceBar = {
+            x: subject_sample_data,
+            y: top_samples_2,
+            text: label,
+            marker: {
+                color: subject_sample.otu_ids,
+                colorscale: "Earth"
+        },
+            type:"bar",
+            orientation: "h",
+        };
+          
+        var dataBar = [traceBar];
+  
+        // define bar chart layout
+        var layoutBar = {
+            title: "Top Ten Operational Taxonomic Units",
+            yaxis:{
+                tickmode:"linear",
+            },
+            margin: {
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 50
+            }
+        };
+  
+        // generate bar chart on bar id in the HTML
+        Plotly.newPlot("bar", dataBar, layoutBar);
+
+    });
+}
+
+
