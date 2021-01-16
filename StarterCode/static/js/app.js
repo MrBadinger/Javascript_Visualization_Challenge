a7757b3f3ae5e3436d41bc3a192b9cc753aebadf
Michael Badinger <mcbadinger@gmail.com>
2021-01-16 09:26:33 -0600

    Develop ReadMe

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -185,8 +185,6 @@ function gauge_chart(id) {
             width: 500,
             height: 400,
             margin: { t: 25, r: 25, l: 25, b: 25 },
-            paper_bgcolor: "lavender",
-            font: { color: "darkblue", family: "Arial" }
           };
           
           Plotly.newPlot('gauge', data, layout);
